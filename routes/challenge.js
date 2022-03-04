82fa85f978e16b20e3aaa55d2442bf423f7972c6
kjs <rednada1708@gmail.com>
2022-03-04 19:44:57 +0900

    challenge router와 authmiddleware 연결

--- routes/challenge.js
+++ routes/challenge.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Challenge = require('../models/challenge');
 const User = require('../models/user');
+const authMiddleware = require('../middlewares/auth-middleware');
 
-//메인 - 검색기능
+//메인 - 검색기능 기능 구현 완료 (1차..정렬기준 마련 필요)
 router.get('/search', async (req, res) => {
     const { title } = req.query;
     const existChallenges = await Challenge.find(
@@ -25,8 +26,13 @@ router.get('/search', async (req, res) => {
         var dateDiff = Math.ceil((end.getTime() - start.getTime()) / (1000 * 3600 * 24));
         if (dateDiff < 0) {
             item['round'] = '라운드 시작 전입니다.';
+            item['status'] = 'before';
+        } else if (dateDiff > 30) {
+            item['round'] = '라운드 종료 되었습니다.';
+            item['status'] = 'complete';
         } else {
             item['round'] = Math.ceil((dateDiff + 1) / 3);
+            item['status'] = 'playing';
         }
         return item;
     });
@@ -35,13 +41,13 @@ router.get('/search', async (req, res) => {
 });
 
 // 카테고리 페이지 목록조회 //body에 userId 담아야..미들웨어 통과 X
-router.get('/category/:categoryId', async (req, res) => {
-    const { userId } = req.body;
+router.get('/category/:categoryId', authMiddleware, async (req, res) => {
+    const { userId } = res.locals.user;
     const { categoryId } = req.params;
     const { length } = req.query;
     let existUser, userLikes;
     if (userId) {
-        existUser = await User.findOne({ email: userId });
+        existUser = await User.findById(userId);
         userLikes = existUser.likes;
     }
     const existChallenges = await Challenge.find(
@@ -65,8 +71,13 @@ router.get('/category/:categoryId', async (req, res) => {
         const dateDiff = Math.ceil((end.getTime() - start.getTime()) / (1000 * 3600 * 24));
         if (dateDiff < 0) {
             item['round'] = '라운드 시작 전입니다.';
+            item['status'] = 'before';
+        } else if (dateDiff > 30) {
+            item['round'] = '라운드 종료 되었습니다.';
+            item['status'] = 'complete';
         } else {
             item['round'] = Math.ceil((dateDiff + 1) / 3);
+            item['status'] = 'playing';
         }
         // isLike 만드는 부분.
         if (userId) {
@@ -78,73 +89,88 @@ router.get('/category/:categoryId', async (req, res) => {
         } else {
             item['isLike'] = false;
         }
-        // if(userId && userLikes.includes(item.challengeId)){
-        //     item["isLike"] = true
-        // } else{
-        //     item["isLike"] = false
-        // }
         return item;
     });
     res.status(200).json({ challenges });
 });
-
-router.get('/challenges/:challengeId', async (req, res) => {
-    const { challengeId } = req.params;
-    const { userId } = req.body;
-    let userLikes;
-    if (userId) {
-        const existUser = await User.findOne({ email: userId });
-        userLikes = existUser.likes;
-    }
-    console.log(userLikes);
-    const challenge = await Challenge.findById(challengeId).lean();
-    console.log(challenge);
-    // 참가회수
-    const joinPeople = challenge.participants;
-    challenge['participants'] = joinPeople.length;
-    // round
-    const start = challenge.startAt;
-    const cur = new Date().toLocaleDateString();
-    const end = new Date(cur);
-    const dateDiff = Math.ceil((end.getTime() - start.getTime()) / (1000 * 3600 * 24));
-    if (dateDiff < 0) {
-        challenge['round'] = '라운드 시작 전입니다.';
-    } else {
-        challenge['round'] = Math.ceil((dateDiff + 1) / 3);
-    }
-    console.log(joinPeople);
-    if (userId) {
-        console.log(joinPeople, userLikes);
-        if (joinPeople.includes(userId)) {
-            challenge['isParticipate'] = true;
+// 상세조회 API
+router.get('/challenges/:challengeId', authMiddleware, async (req, res) => {
+    try {
+        const { challengeId } = req.params;
+        let userId;
+        if (!res.locals.user) {
+            userId = '';
         } else {
-            challenge['isParticipate'] = false;
+            userId = res.locals.user.userId;
         }
-        if (userLikes.includes(challengeId)) {
-            challenge['isLike'] = true;
+        let userLikes, userParticipate;
+        if (userId) {
+            const existUser = await User.findById(userId);
+            userLikes = existUser.likes;
+            userParticipate = existUser.participate;
+        }
+
+        const challenge = await Challenge.findById(challengeId).lean();
+        // 참가회수
+        console.log(challenge);
+        const joinPeople = challenge.participants;
+        challenge['participants'] = joinPeople.length;
+        // round
+        const start = challenge.startAt;
+        const cur = new Date().toLocaleDateString();
+        const end = new Date(cur);
+        const dateDiff = Math.ceil((end.getTime() - start.getTime()) / (1000 * 3600 * 24));
+        if (dateDiff < 0) {
+            challenge['round'] = '라운드 시작 전입니다.';
+            challenge['status'] = 'before';
+        } else if (dateDiff > 30) {
+            challenge['round'] = '라운드 종료 되었습니다.';
+            challenge['status'] = 'complete';
+        } else {
+            challenge['round'] = Math.ceil((dateDiff + 1) / 3);
+            challenge['status'] = 'playing';
+        }
+        if (userId) {
+            console.log(userId, joinPeople);
+            // if (joinPeople.includes(userId)){ 이걸로 하면 왜 안되는지..ㅜ.ㅜ
+            if (userParticipate.includes(challengeId)) {
+                challenge['isParticipate'] = true;
+            } else {
+                challenge['isParticipate'] = false;
+            }
+            if (userLikes.includes(challengeId)) {
+                challenge['isLike'] = true;
+            } else {
+                challenge['isLike'] = false;
+            }
+            // 로그인한 유저의 참여여부
+            // 로그인한 유저의 좋아요여부
         } else {
             challenge['isLike'] = false;
+            challenge['isParticipate'] = false;
+            challenge['isUpload'] = false;
+            challenge['proofCount'] = false;
         }
-        // 로그인한 유저의 참여여부
-        // 로그인한 유저의 좋아요여부
-    } else {
-        challenge['isLike'] = false;
-        challenge['isParticipate'] = false;
-        challenge['isUpload'] = false;
-        challenge['proofCount'] = false;
+        res.status(200).send(challenge);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ message: err.message });
     }
-
-    res.send(challenge);
 });
 
-//챌린지 작성 API //미들웨어 통과시켜야함
-router.post('/challenges', async (req, res) => {
-    // 로그인 기능 완료 되면 authMiddleware 통과시키기!!
+//챌린지 작성 API 기능 구현 완료
+router.post('/challenges', authMiddleware, async (req, res) => {
     try {
-        //const userId = "1234" // authmiddleware에서 가져올 예정
-        const { userId, title, content, category, thumbnail, startAt, howtoContent } = req.body;
+        if (!res.locals.user) {
+            res.status(401).send({
+                errorMessage: '로그인 후 사용하시오',
+            });
+            return;
+        }
+        const { userId } = res.locals.user;
+        const { title, content, category, thumbnail, startAt, howtoContent } = req.body;
         const participants = [userId];
-        const existUser = await User.findOne({ email: userId });
+        const existUser = await User.findById(userId);
         const participate = existUser.participate;
         const createdChallenge = await Challenge.create({
             title,
@@ -157,7 +183,7 @@ router.post('/challenges', async (req, res) => {
         });
         const challengeId = createdChallenge.challengeId;
         participate.push(challengeId);
-        await User.updateOne({ email: userId }, { $set: { participate } });
+        await User.updateOne({ _id: userId }, { $set: { participate } });
         res.status(201).json({ message: '챌린지 작성이 완료되었습니다.' }); // created : 201
     } catch (err) {
         console.log(err);
@@ -165,22 +191,27 @@ router.post('/challenges', async (req, res) => {
     }
 });
 
-// 챌린지 참여하기 기능.. user에서 추가해줘야 되네.. //미들웨어 통과시켜야함
-router.post('/challenges/:challengeId/join', async (req, res) => {
+// 챌린지 참여하기 기능 구현 완료
+router.post('/challenges/:challengeId/join', authMiddleware, async (req, res) => {
     //일단 challengeId로 조회해야함
-    const { userId } = req.body; //authmiddleware에서 가져올 예정
-    console.log(userId);
+    if (!res.locals.user) {
+        res.status(401).send({
+            errorMessage: '로그인 후 사용하시오',
+        });
+        return;
+    }
+    const { userId } = res.locals.user;
     const { challengeId } = req.params;
     const existChallenge = await Challenge.findById(challengeId);
     const participants = existChallenge.participants;
-    const existUser = await User.findOne({ email: userId });
+    const existUser = await User.findById(userId);
     const participate = existUser.participate;
     if (!participants.includes(userId)) {
         participants.push(userId);
         await Challenge.updateOne({ _id: challengeId }, { $set: { participants } });
         if (!participate.includes(challengeId)) {
             participate.push(challengeId);
-            await User.updateOne({ email: userId }, { $set: { participate } });
+            await User.updateOne({ _id: userId }, { $set: { participate } });
         }
         res.status(201).json({ message: '참여성공' });
     } else {
@@ -188,22 +219,26 @@ router.post('/challenges/:challengeId/join', async (req, res) => {
     }
 });
 
-// 챌린지 참여취소 기능.. user에서 삭제해줘야 되네.. //미들웨어 통과시켜야함
-router.delete('/challenges/:challengeId/join', async (req, res) => {
-    //일단 challengeId로 조회해야함
-    const { userId } = req.body; //authmiddleware에서 가져올 예정
-    console.log(userId);
+// 챌린지 참여취소 기능 구현 완료
+router.delete('/challenges/:challengeId/join', authMiddleware, async (req, res) => {
+    if (!res.locals.user) {
+        res.status(401).send({
+            errorMessage: '로그인 후 사용하시오',
+        });
+        return;
+    }
+    const { userId } = res.locals.user;
     const { challengeId } = req.params;
     const existChallenge = await Challenge.findById(challengeId);
     const participants = existChallenge.participants;
-    const existUser = await User.findOne({ email: userId });
+    const existUser = await User.findById(userId);
     const participate = existUser.participate;
     if (participants.includes(userId)) {
         participants.splice(participants.indexOf(userId), 1);
         await Challenge.updateOne({ _id: challengeId }, { $set: { participants } });
         if (participate.includes(challengeId)) {
             participate.splice(participate.indexOf(challengeId), 1);
-            await User.updateOne({ email: userId }, { $set: { participate } });
+            await User.updateOne({ _id: userId }, { $set: { participate } });
         }
         res.status(200).json({ message: '참여 취소 성공' });
     } else {
@@ -211,32 +246,42 @@ router.delete('/challenges/:challengeId/join', async (req, res) => {
     }
 });
 
-//찜하기 기능 완성 //미들웨어 통과시켜야함
-router.post('/challenges/:challengeId/like', async (req, res) => {
-    //일단 challengeId로 조회해야함
-    const { userId } = req.body; //authmiddleware에서 가져올 예정
+//찜하기 기능 구현 완료
+router.post('/challenges/:challengeId/like', authMiddleware, async (req, res) => {
+    if (!res.locals.user) {
+        res.status(401).send({
+            errorMessage: '로그인 후 사용하시오',
+        });
+        return;
+    }
+    const { userId } = res.locals.user;
     const { challengeId } = req.params;
-    const existUser = await User.findOne({ email: userId });
+    const existUser = await User.findById(userId);
     const existLikes = existUser.likes;
     if (!existLikes.includes(challengeId)) {
         existLikes.push(challengeId);
-        await User.updateOne({ email: userId }, { $set: { likes: existLikes } });
+        await User.updateOne({ _id: userId }, { $set: { likes: existLikes } });
         res.status(201).json({ message: '찜하기 성공' });
     } else {
         res.status(400).json({ message: '찜하기 실패' });
     }
 });
 
-// 찜하기 취소 기능 완성 //미들웨어 통과시켜야함
-router.delete('/challenges/:challengeId/like', async (req, res) => {
-    //일단 challengeId로 조회해야함
-    const { userId } = req.body; //authmiddleware에서 가져올 예정
+// 찜하기 취소 기능 구현 완료
+router.delete('/challenges/:challengeId/like', authMiddleware, async (req, res) => {
+    if (!res.locals.user) {
+        res.status(401).send({
+            errorMessage: '로그인 후 사용하시오',
+        });
+        return;
+    }
+    const { userId } = res.locals.user;
     const { challengeId } = req.params;
-    const existUser = await User.findOne({ email: userId });
+    const existUser = await User.findById(userId);
     const existLikes = existUser.likes;
     if (existLikes.includes(challengeId)) {
         existLikes.splice(existLikes.indexOf(challengeId), 1);
-        await User.updateOne({ email: userId }, { $set: { likes: existLikes } });
+        await User.updateOne({ _id: userId }, { $set: { likes: existLikes } });
         res.status(200).json({ message: '찜하기 취소 성공' });
     } else {
         res.status(400).json({ message: '찜하기 취소 실패' });
