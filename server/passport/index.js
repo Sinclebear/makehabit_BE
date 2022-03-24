ac9d81ea392f280b2fcfdd844209930a652b2b80
Sinclebear <sanghyukj@gmail.com>
2022-03-24 20:19:22 +0900

    🔒️ 카카오톡 소셜 로그인 callback url 변경

--- server/passport/index.js
+++ server/passport/index.js
@@ -9,8 +9,7 @@ module.exports = () => {
         new KakaoStrategy(
             {
                 clientID: process.env.KAKAO_ID, // 카카오 로그인에서 발급받은 REST API 키
-                // callbackURL: 'http://52.79.227.179/api/users/kakao/callback', // 카카오 로그인 Redirect URI 경로
-                callbackURL: 'http://localhost:3000/kakao', // 카카오 로그인 Redirect URI 경로
+                callbackURL: process.env.KAKAO_CALLBACK_URL, // 카카오 로그인 Redirect URI 경로
             },
 
             // clientID에 카카오 앱 아이디 추가
