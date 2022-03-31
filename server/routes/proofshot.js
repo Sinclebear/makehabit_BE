510c0cc557caafcb3c7297622e80cc7c5f867e3d
Sinclebear <sanghyukj@gmail.com>
2022-04-01 00:11:40 +0900

    ✨ 인증샷 수정 기능 추가

--- server/routes/proofshot.js
+++ server/routes/proofshot.js
@@ -4,10 +4,13 @@ const authMiddleware = require('../middlewares/auth-middleware');
 const proofShotCtl = require('../controller/proofshot');
 // /api/proofshot
 
-// 인증페이지 - 인증페이지 조회 (챌린지 상세페이지 조회와 동일. 필요한 요소만 뽑아서 보내도록 수정.)
+// 인증페이지 - 인증페이지 조회
 router.get('/:challengeId/auth', authMiddleware, proofShotCtl.authProofshot);
 
-// 인증페이지 - 인증 완료 (업로드). 작성 필요
+// 인증페이지 - 인증 완료 (업로드).
 router.post('/:challengeId/proof', authMiddleware, proofShotCtl.uploadProofshot);
+
+// 인증페이지 - 인증 완료 (업로드).
+router.patch('/:proofshotId', authMiddleware, proofShotCtl.modifyProofshot);
 
 module.exports = router;
